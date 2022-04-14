3a8261f9261175c2844b94c49ae9089313685d00
Malinx95 <grodet95@gmail.com>
2022-04-14 15:18:58 +0200

    added discord like sidebar

--- src/Chevre.jsx
+++ src/Chevre.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const chevre = ({chevre, onChange}) => {
 
     return (
-        <div style={chevre.selected ? {boxShadow:'0 0 10px rgb(255 0 0 / 50%)'} : {}} className="chevre" onClick={() => onChange(chevre.id)}>
+        <div className="chevre" onClick={() => onChange(chevre.id)}>
+            <img style={chevre.selected ? {boxShadow:'0 0 10px rgb(255 0 0 / 50%)'} : {}} src={chevre.picture} alt="pp" />
             <p> {chevre.nom} {chevre.prenom}</p>
-            <p>age : {chevre.age}</p>
-            <p>selected : {chevre.selected.toString()}</p>
+            <p>{chevre.age}</p>
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -1,18 +1,46 @@
-import React from 'react';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import ChevreSelector from './ChevreSelector';
+import React, { useState } from "react";
 import { createRoot } from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import './index.css';
+import ChevreSelector from './ChevreSelector';
+import CV from "./CV";
+import ppmalinx from './images/ppmalinx.png';
+import ppchevre from './images/chevre.png';
 
 const App = () => {
 
+    const [chevres, setChevres] = useState([
+        {id: 0, selected: true, nom : "Acceuil", prenom : "", age : "", picture : ppchevre},
+        {id: 1, selected: false, nom : "Grodet", prenom : "Maxime", age : "22 ans", picture : ppmalinx},
+        {id: 2, selected: false, nom : "Ait-Mansour", prenom : "Yassin", age : "22 ans", picture : ""},
+    ]);
+    
+
+    const [selectedChevre, setSelectedChevre] = useState(0);
+
+    const handleSelect = (id) => {
+        const updatedChevres = [...chevres];
+        updatedChevres.forEach(chevre => {
+            if (chevre.id === id) {
+                chevre.selected = true;
+            }
+            else{
+                chevre.selected = false;
+            }
+        });
+        setChevres([...updatedChevres]);
+        setSelectedChevre(id);
+    };
 
     return (
         <div>
-            <h1>Les Chèvres</h1>
-            <h2>Qui sommes nous ?</h2>
-            <p>Paragraphe de presentation du collectif de BG que nous sommes !</p>
-            <ChevreSelector />
+            <ChevreSelector chevres={chevres} handleSelect={handleSelect} />
+            <div className='content'>
+                <h1>Les Chèvres</h1>
+                <h2>Qui sommes nous ?</h2>
+                <p>Paragraphe de presentation du collectif de BG que nous sommes !</p>
+                <CV id={selectedChevre} />
+            </div>
         </div>
     );
 };
