42ed626ee17268d3ad741a2e1763c598cd3817e7
blovorad <62188572+blovorad@users.noreply.github.com>
2022-04-14 15:33:01 +0200

    Update CV.jsx
    added cv gautier

--- src/CV.jsx
+++ src/CV.jsx
@@ -4,6 +4,7 @@ const CV = ({id}) => {
     const CV = useState([
         {id: 1, txt: "cv a malinx"},
         {id: 2, txt: "cv a yassin"},
+        {id: 3, txt: "cv a gautier"},
     ])[0];
 
     if(id != null && id !== 0) {
